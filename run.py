4e60a462a759c1fba9d96e1d52e2b5b07641039d
soumyatanwar <37302163+soumyatanwar@users.noreply.github.com>
2019-10-21 15:52:28 +0530

    Changed script argument defaults
    in order to see if the code could be invoked directly in the Jupyter cell instead of depending on the script calling so that line magic works straight out of the box

--- run.py
+++ run.py
@@ -23,9 +23,9 @@ logger.addHandler(ch)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation run')
     parser.add_argument('--image', type=str, default='./images/p1.jpg')
-    parser.add_argument('--model', type=str, default='cmu',
+    parser.add_argument('--model', type=str, default='mobilenet_thin',
                         help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
-    parser.add_argument('--resize', type=str, default='0x0',
+    parser.add_argument('--resize', type=str, default='432x368',
                         help='if provided, resize images before they are processed. '
                              'default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
     parser.add_argument('--resize-out-ratio', type=float, default=4.0,
